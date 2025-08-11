e83621fd375cbc56a201dd1b25437c06fadc3f1c
mariah.lightfoot <mariah.lightfoot@capacity.com>
2025-08-10 23:06:34 -0500

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/5th_day_react_app/', // Replace with your repository name
+  base: '/5th_day_app_v1/', // Add this line - should match your repository name
 }) 
